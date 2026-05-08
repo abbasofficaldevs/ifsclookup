685c9b1cf4124ef73c6442af8e9b825ad76715d7
ABBAS <abbasofficaldevs@gmail.com>
2026-05-08 07:15:23 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="ifsclookup",
-    version="1.0.2",
+    version="1.0.1",
     author="abbas",
     author_email="abbasofficialdevs@gmail.com",
     description="Simple Python package for Indian IFSC lookup",

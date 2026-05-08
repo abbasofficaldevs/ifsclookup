ffe2f12cbb0144610670e9350fffc56226ef3ac1
ABBAS <abbasofficaldevs@gmail.com>
2026-05-08 06:12:59 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="ifsclookup",
     version="1.0.0",
     author="abbas",
     author_email="abbasofficialdevs@gmail.com",
     description="Simple Python package for Indian IFSC lookup",
-    long_description=open("README.md", encoding="utf-8").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/abbasofficaldevs/ifsclookup",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
